d59afa28c6cf4ccd355f98b2eb714aaa7e96bbeb
jack <jackma790@gmail.com>
2025-04-14 22:01:55 +1000

    adding fossils to list of fetch trade item listings

---
+++ POEBulkTradeService/src/BulkTradeService/Enums/Fossil.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.Serialization;
+
+namespace WorkerService1.BulkTradeService.Enums;
+
+public enum Fossil
+{
+    [EnumMember(Value = "frigid-fossil")] FrigidFossil,
+    [EnumMember(Value = "scorched-fossil")] ScorchedFossil,
+    [EnumMember(Value = "metallic-fossil")] MetallicFossil,
+    [EnumMember(Value = "jagged-fossil")] JaggedFossil,
+    [EnumMember(Value = "aberrant-fossil")] AberrantFossil,
+    [EnumMember(Value = "pristine-fossil")] PristineFossil,
+    [EnumMember(Value = "dense-fossil")] DenseFossil,
+    [EnumMember(Value = "corroded-fossil")] CorrodedFossil,
+    [EnumMember(Value = "prismatic-fossil")] PrismaticFossil,
+    [EnumMember(Value = "aetheric-fossil")] AethericFossil,
+    [EnumMember(Value = "serrated-fossil")] SerratedFossil,
+    [EnumMember(Value = "lucent-fossil")] LucentFossil,
+    [EnumMember(Value = "shuddering-fossil")] ShudderingFossil,
+    [EnumMember(Value = "bound-fossil")] BoundFossil,
+    [EnumMember(Value = "perfect-fossil")] PerfectFossil,
+    [EnumMember(Value = "deft-fossil")] DeftFossil,
+    [EnumMember(Value = "fundamental-fossil")] FundamentalFossil,
+    [EnumMember(Value = "faceted-fossil")] FacetedFossil,
+    [EnumMember(Value = "bloodstained-fossil")] BloodstainedFossil,
+    [EnumMember(Value = "hollow-fossil")] HollowFossil,
+    [EnumMember(Value = "fractured-fossil")] FracturedFossil,
+    [EnumMember(Value = "glyphic-fossil")] GlyphicFossil,
+    [EnumMember(Value = "tangled-fossil")] TangledFossil,
+    [EnumMember(Value = "sanctified-fossil")] SanctifiedFossil,
+    [EnumMember(Value = "gilded-fossil")] GildedFossil,
+    [EnumMember(Value = "enchanted-fossil")] EnchantedFossil,
+    [EnumMember(Value = "primeval-fossil")] PrimevalFossil
+}
